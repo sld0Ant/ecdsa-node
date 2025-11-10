21e37ff7affc7aef780a1928f9313e5cb50abce9
sld0ant <sld0ant@gmail.com>
2025-11-10 22:36:42 +0300

    feat(): inital + signature verify on serv

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,23 +1,63 @@
 import { useState } from "react";
 import server from "./server";
-
+import { sign, verify, getPublicKey } from "ethereum-cryptography/secp256k1";
+import { sha256 } from "ethereum-cryptography/sha256.js";
+import {
+  bytesToHex as toHex,
+  hexToBytes as toBytes,
+} from "ethereum-cryptography/utils.js";
+import { utf8ToBytes } from "ethereum-cryptography/utils.js";
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
+  const singTransaction = async (transaction, privateKey) => {
+    const txHash = sha256(utf8ToBytes(JSON.stringify(transaction)));
+    const txHashHex = toHex(txHash);
+    const txHashBytes = toBytes(txHashHex);
+
+    const signature = await sign(txHashBytes, toBytes(privateKey));
+    console.log(
+      verify(signature, txHashBytes, getPublicKey(privateKey)),
+      signature,
+      txHashBytes,
+      getPublicKey(privateKey)
+    );
+    return { signature: toHex(signature), txHash: toHex(txHash) };
+  };
+
+  const buildTransaction = async (data, privateKey) => {
+    const sign = await singTransaction(data, privateKey);
+    return { ...data, ...sign };
+  };
   async function transfer(evt) {
     evt.preventDefault();
-
+    console.log(
+      await buildTransaction(
+        {
+          sender: address,
+          amount: parseInt(sendAmount),
+          recipient,
+        },
+        privateKey
+      )
+    );
     try {
+      const transaction = await buildTransaction(
+        {
+          sender: address,
+          amount: parseInt(sendAmount),
+          recipient,
+        },
+        privateKey
+      );
+      console.log(transaction);
       const {
         data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
+      } = await server.post(`send`, transaction);
       setBalance(balance);
     } catch (ex) {
       alert(ex.response.data.message);
@@ -43,6 +83,15 @@ function Transfer({ address, setBalance }) {
           placeholder="Type an address, for example: 0x2"
           value={recipient}
           onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+
+      <label>
+        Private key
+        <input
+          placeholder="Type private key"
+          value={privateKey}
+          onChange={setValue(setPrivateKey)}
         ></input>
       </label>
 
--- server/index.js
+++ server/index.js
@@ -1,44 +1,74 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { generateKeys, verify } = require("./generate-keys.ts");
+const { utf8ToBytes, hexToBytes } = require("ethereum-cryptography/utils");
+
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+const users = Array(2)
+  .fill(0)
+  .map(() => ({
+    ...generateKeys(),
+    balance: Math.floor(Math.random() * 100),
+  }));
+
+const getUser = (address) => users.find((user) => user.id === address);
+
+const isUserExist = (address) => getUser(address);
+
+const hasEnoughBalance = (address, amount) =>
+  getUser(address)?.balance > amount;
+
+const withdraw = (from, to, sum) => {
+  getUser(from).balance -= sum;
+  getUser(to).balance += sum;
 };
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
+
+  res.send({ balance: getUser(address)?.balance || "Wallet not found" });
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, txHash, signature } = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
-
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+  console.log(
+    verify(
+      hexToBytes(signature),
+      hexToBytes(txHash),
+      getUser(sender).publicKey
+    ),
+    hexToBytes(signature),
+    hexToBytes(txHash),
+    getUser(sender).publicKey
+  );
+  if (
+    !verify(
+      hexToBytes(signature),
+      hexToBytes(txHash),
+      getUser(sender).publicKey
+    )
+  ) {
+    return res.status(400).send({ message: "Malicious signature" });
   }
+  if (!isUserExist(recipient)) {
+    return res.status(400).send({ message: "Invalid wallet" });
+  }
+  if (!hasEnoughBalance(sender, amount)) {
+    return res.status(400).send({ message: "Not enough funds!" });
+  }
+
+  withdraw(sender, recipient, amount);
+  res.send({ balance: getUser(sender).balance });
+
+  console.log(getUser(sender).balance, getUser(recipient).balance);
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
-
-function setInitialBalance(address) {
-  if (!balances[address]) {
-    balances[address] = 0;
-  }
-}
